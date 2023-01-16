8e5b7ce8a4b2c08b23bd7795ae61c7e91b176963
CrypticMessenger <2020csb1072@iitrpr.ac.in>
2023-01-16 16:24:48 +0530

    persist login

--- server.js
+++ server.js
@@ -10,18 +10,7 @@ const client = new MongoClient(uri);
 client.connect();
 const collection = client.db("test").collection('otp');
 const collection_users = client.db("current_users").collection('users');
-// collection.insertOne({'email':'ankitsharma61016@gmail.com','otp':1234});
-// collection.findOne({'email':'ankitsharma61016@gmail.com'},(err,data)=>{
-//   if(err){
-//     console.log("an error occured");
-//   }
-//   else if(!data){
-//     console.log("no object present!")
-//   }
-//   else{
-//     console.log("otp is: ",data.otp)
-//   }
-// });
+
 
 // Create a transport object to configure the email server
 const transport = nodemailer.createTransport({
@@ -33,37 +22,6 @@ const transport = nodemailer.createTransport({
 });
 app.use(cors());
 
-app.post('/api/otp',(req,res)=>{
-    
-    collection.findOne({'email':req.body.email},(err,data)=>{
-      if(err){
-        console.log("an error occured");
-        res.send({success:false,otp_sent:false})
-      }
-      else if(!data){
-        let otp = Math.floor(1000 + Math.random() * 9000);
-        
-        collection.insertOne({'email':req.body.email,'otp':otp})
-        const mailOptions = {
-          from: 'ankit13112002.sharma@gmail.com', // replace with the sender's email address
-          to: req.body.email, // replace with the recipient's email address
-          subject: 'OTP for authentication', // replace with the email subject
-          text: `your OTP for authentication is ${otp}` // replace with the email body
-        };
-        transport.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            res.status(500).send({success:false,otp_sent:false});
-        } else {
-            res.send({success: true,otp_sent:true});
-        }
-        });
-      }
-      else{
-        res.send({success:true,otp_sent:false})
-      }
-    });
-    
-})
 
 app.post('/api/login',async  (req, res) => {
   const email = req.body.data.email;
@@ -79,21 +37,7 @@ app.post('/api/login',async  (req, res) => {
 
 })
 
-app.get('/api/auth',(req, res) => {
-  const email = req.body.email;
-  const otp = req.body.password; 
-  collection.findOne({ email: email,otp:otp},(err,data)=>{
-    if(err){
-      res.send({success:false})
-    }
-    else if(!data){
-      res.send({success:false})
-    }
-    else{
-      res.send({success:true})
-    }
-  })
-})
+
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
