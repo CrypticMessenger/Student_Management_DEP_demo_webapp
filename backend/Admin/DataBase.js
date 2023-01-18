c95512b418fc7e8e7e175190f35fe3ad680ab41f
Blue-coder89 <nish95.sha@gmail.com>
2023-01-18 10:32:02 +0530

    OTP authentication done

---
+++ backend/Admin/DataBase.js
@@ -0,0 +1,5 @@
+const password = "so2QY5f6GdxnLoXJ"
+const DataBase = "College";
+const studentCollection = "students";
+const URI = `mongodb+srv://user:${password}@cluster0.6uvcoow.mongodb.net/?retryWrites=true&w=majority`
+export {URI,studentCollection,DataBase};
