269d576222f5241de6d988c6023f73ad7fb2a423
Ankit Sharma <75074904+CrypticMessenger@users.noreply.github.com>
2023-05-14 17:46:38 +0530

    Update DataBase.js

--- backend/Admin/DataBase.js
+++ backend/Admin/DataBase.js
@@ -1,4 +1,4 @@
-const password = "so2QY5f6GdxnLoXJ"
+const password = "-"
 const DataBase = "current_users";
 const studentCollection = "users";
 const URI = `mongodb+srv://user:${password}@cluster0.6uvcoow.mongodb.net/?retryWrites=true&w=majority`
